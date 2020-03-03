9fe4ab95bbfa9269a3682fe83710a7bca77eb85b
maitandat1507 <maitandat1507@gmail.com>
2020-03-03 20:15:32 +0700

    Param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,18 @@ const fs = require('fs')
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`)
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+  next()
+}
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime)
 
@@ -19,14 +31,6 @@ exports.getTour = (req, res) => {
   console.log(req.params)
   const id = req.params.id * 1 // TIP: convert STRING into NUMBER
   const tour = tours.find(el => el.id === id)
-
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }
 
   res.status(200).json({
     status: 'success',
@@ -69,13 +73,6 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }
-
   res.status(204).json({
     status: 'success',
     data: null
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,13 @@ const tourController = require('./../controllers/tourController')
 
 const router = express.Router()
 
+router.param('id', tourController.checkID)
+
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (Bad request)
+// Add it to the post handler stack
+
 router.route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour)
