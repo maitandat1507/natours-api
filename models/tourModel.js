4e055606df5044642286fa04db52a83492f2d360
maitandat1507 <maitandat1507@gmail.com>
2020-05-10 20:44:58 +0700

    Lecture 104:  middleware (1/4 types of middleware in Mongoose)

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true 
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration.']
@@ -60,6 +62,25 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7  // `this` keyword will point to CURRENT Document
 })
+
+
+// DOCUMENT MIDDLEWARE: runs BEFORE .save() and .create()    (NOT AFFECTED for .insert())
+tourSchema.pre('save', function(next) {
+  // console.log(this) // `this` - point to currently process document
+
+  this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...')
+//   next()
+// })
+
+// tourSchema.post('save', function(doc, next) { // `doc` - the document just saved right before
+//   console.log(doc)
+//   next()
+// })
 
 const Tour = mongoose.model('Tour', tourSchema)
 
